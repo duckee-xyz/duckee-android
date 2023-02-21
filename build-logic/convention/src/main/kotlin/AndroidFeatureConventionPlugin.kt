975f319ef6b2d1b84536ef2ea0f958da691ca83f
Dora Lee <dsa28s@naver.com>
2023-02-22 02:43:48 +0900

    build: add android feature convention plugin

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,23 @@
+@file:Suppress("Unused")
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("duckee.android.library")
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            dependencies {
+                "implementation"(libs.findLibrary("kotlin.coroutines").get())
+            }
+        }
+    }
+}
