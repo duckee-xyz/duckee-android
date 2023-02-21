b6a8ffdbd9f4ab213e36bbcd84040d22d2075b0e
Dora Lee <dsa28s@naver.com>
2023-02-22 02:43:25 +0900

    build: add android library compose convention plugin

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
@@ -0,0 +1,17 @@
+@file:Suppress("Unused")
+
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import xyz.duckee.android.configureAndroidCompose
+
+class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.android.library")
+            val extension = extensions.getByType<LibraryExtension>()
+            configureAndroidCompose(extension)
+        }
+    }
+}
