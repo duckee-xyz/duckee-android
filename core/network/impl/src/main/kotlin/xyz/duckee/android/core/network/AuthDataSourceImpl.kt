34ddf06ccbff6bb8a199f49ae8145d4cc04b0be7
Dora Lee <dsa28s@naver.com>
2023-02-25 01:23:04 +0900

    feat(network): add auth datasource (include firebase authentication)

---
+++ core/network/impl/src/main/kotlin/xyz/duckee/android/core/network/AuthDataSourceImpl.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 The Duckee Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.duckee.android.core.network
+
+import com.skydoves.sandwich.ApiResponse
+import xyz.duckee.android.core.network.api.AuthAPI
+import xyz.duckee.android.core.network.firebase.FirebaseAuthManager
+import xyz.duckee.android.core.network.model.ResponseSignIn
+import xyz.duckee.android.core.network.model.request.RequestSignIn
+import javax.inject.Inject
+
+internal class AuthDataSourceImpl @Inject constructor(
+    apiProvider: APIProvider,
+    private val firebaseAuthManager: FirebaseAuthManager,
+) : AuthDataSource {
+
+    private val api = apiProvider[AuthAPI::class.java]
+
+    override suspend fun signInWithFirebase(): ApiResponse<ResponseSignIn> {
+        val idToken = firebaseAuthManager.getCurrentUserIdToken().orEmpty()
+
+        if (idToken.isBlank()) {
+            return ApiResponse.error(NullPointerException("firebase idToken == null"))
+        }
+
+        return api.signIn(
+            payload = RequestSignIn(
+                channel = "firebase",
+                token = idToken,
+            ),
+        )
+    }
+
+    override suspend fun signUpWithFirebase(): ApiResponse<ResponseSignIn> {
+        val idToken = firebaseAuthManager.getCurrentUserIdToken().orEmpty()
+
+        if (idToken.isBlank()) {
+            return ApiResponse.error(NullPointerException("firebase idToken == null"))
+        }
+
+        return api.signUp(
+            payload = RequestSignIn(
+                channel = "firebase",
+                token = idToken,
+            ),
+        )
+    }
+}
---
+++ core/network/impl/src/main/kotlin/xyz/duckee/android/core/network/di/NetworkModule.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023 The Duckee Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.duckee.android.core.network.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.skydoves.sandwich.adapters.ApiResponseCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import xyz.duckee.android.core.network.APIProvider
+import xyz.duckee.android.core.network.APIProviderImpl
+
+@Suppress("unused")
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        prettyPrint = true
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideKotlinSerializationConverterFactory(
+        json: Json,
+    ): Converter.Factory =
+        json.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideApiResponseCallAdapterFactory(): ApiResponseCallAdapterFactory {
+        return ApiResponseCallAdapterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLogInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun providesApiProvider(
+        apiProviderImpl: APIProviderImpl,
+    ): APIProvider = apiProviderImpl
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory,
+        apiResponseCallAdapterFactory: ApiResponseCallAdapterFactory,
+    ): Retrofit = Retrofit.Builder().apply {
+        client(okHttpClient)
+
+        baseUrl(xyz.duckee.android.core.network.api.BuildConfig.API_BASE_URL)
+        addConverterFactory(converterFactory)
+        addCallAdapterFactory(apiResponseCallAdapterFactory)
+    }.build()
+}
