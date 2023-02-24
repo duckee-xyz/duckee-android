fbca9c5536dae1dba54d3e5fec7d319bc4c0e56f
Dora Lee <dsa28s@naver.com>
2023-02-24 22:52:59 +0900

    build: add network related module, serialization plugin

--- build-logic/convention/src/main/kotlin/AndroidSerializationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidSerializationConventionPlugin.kt
@@ -20,7 +20,7 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
-@Suppress("UnstableApiUsage")
+@Suppress("UnstableApiUsage", "Unused")
 class AndroidSerializationConventionPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
