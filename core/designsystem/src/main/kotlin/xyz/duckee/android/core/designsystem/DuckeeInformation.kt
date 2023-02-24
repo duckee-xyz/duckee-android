dcab8ccf1535c700e256f04ecacae73bbb3cd6fc
Dora Lee <dsa28s@naver.com>
2023-02-25 06:19:38 +0900

    feat(design-system): add DuckeeInformation component

---
+++ core/designsystem/src/main/kotlin/xyz/duckee/android/core/designsystem/DuckeeInformation.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 The Duckee Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.duckee.android.core.designsystem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import xyz.duckee.android.core.designsystem.theme.DuckeeTheme
+
+@Composable
+fun DuckeeInformation(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = modifier
+            .padding(horizontal = 24.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color(0xff2A333A))
+            .padding(horizontal = 20.dp, vertical = 16.dp),
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun DuckeeInformationItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    value: String,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = title,
+            style = DuckeeTheme.typography.paragraph4,
+            fontWeight = FontWeight.ExtraLight,
+            color = Color(0xFFFBFBFB),
+        )
+        Text(
+            text = value,
+            style = DuckeeTheme.typography.paragraph4,
+            fontWeight = FontWeight.ExtraLight,
+            color = Color(0xFFACB7BF),
+        )
+    }
+}
