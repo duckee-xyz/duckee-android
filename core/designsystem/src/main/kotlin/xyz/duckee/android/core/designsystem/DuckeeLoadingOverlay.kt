c6ba8a5238c07bed22d11791e51b5d3c2a19fe1b
Dora Lee <dsa28s@naver.com>
2023-02-25 22:33:16 +0900

    feat(design-system): add DuckeeCharacterLoadingOverlay component

--- core/designsystem/src/main/kotlin/xyz/duckee/android/core/designsystem/DuckeeLoadingOverlay.kt
+++ core/designsystem/src/main/kotlin/xyz/duckee/android/core/designsystem/DuckeeLoadingOverlay.kt
@@ -15,17 +15,29 @@
  */
 package xyz.duckee.android.core.designsystem
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import xyz.duckee.android.core.designsystem.theme.DuckeeTheme
 
 @Composable
 fun DuckeeLoadingOverlay() {
@@ -44,6 +56,57 @@ fun DuckeeLoadingOverlay() {
                     .size(48.dp)
                     .align(Alignment.Center),
             )
+        }
+    }
+}
+
+@Composable
+fun DuckeeCharacterLoadingOverlay(
+    loadingMessage: String = "Loading...",
+) {
+    Dialog(
+        properties = DialogProperties(
+            dismissOnBackPress = false,
+            dismissOnClickOutside = false,
+        ),
+        onDismissRequest = {},
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {},
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(28.dp),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(450.dp)
+                        .offset(y = 150.dp)
+                        .pointerInput(Unit) {},
+                ) {
+                    val composition by rememberLottieComposition(
+                        LottieCompositionSpec.RawRes(R.raw.animation_loading_duckee),
+                    )
+                    val progress by animateLottieCompositionAsState(
+                        composition,
+                        iterations = LottieConstants.IterateForever,
+                    )
+
+                    LottieAnimation(
+                        composition = composition,
+                        progress = { progress },
+                        modifier = Modifier
+                            .align(Alignment.Center),
+                    )
+                }
+                Text(
+                    text = loadingMessage,
+                    style = DuckeeTheme.typography.h5,
+                    color = Color.White,
+                )
+            }
         }
     }
 }
